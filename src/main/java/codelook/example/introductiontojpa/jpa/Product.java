71e26ff69891df8fc34c50b3d77d4bdfdaa4905c
Babak <babak@sce.carleton.ca>
2023-09-17 03:34:33 -0400

    upgraded javax dependencies and packages to jakarta

--- src/main/java/codelook/example/introductiontojpa/jpa/Product.java
+++ src/main/java/codelook/example/introductiontojpa/jpa/Product.java
@@ -1,7 +1,7 @@
 package codelook.example.introductiontojpa.jpa;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 @Entity
 public class Product {
